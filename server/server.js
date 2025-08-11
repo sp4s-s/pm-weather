3492adf77e7672fcedf9013cb159fc80758dfe10
spass <168576652+sp4s-s@users.noreply.github.com>
2025-08-11 22:02:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,57 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const { initDB } = require('./db')
-const routes = require('./routes')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { initDB } = require('./db');
+const routes = require('./routes');
+const serverless = require('serverless-http');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use('/', routes)
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
 
-initDB().catch(err => {
-  console.error('DB init error:', err)
-  process.exit(1)
-})
+// Vercel runs a new instance per invocation, but for pooled DBs you may want "singleton" init
+let dbInitPromise;
+app.use((req, res, next) => {
+  if (!dbInitPromise) {
+    dbInitPromise = initDB().catch(err => {
+      console.error('DB init error:', err);
+      // Don't process.exit
+      throw err;
+    });
+  }
+  dbInitPromise.then(() => next()).catch(next);
+});
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`running on ${PORT}`))
+module.exports = serverless(app);
+
+
+
+
+
+
+
+
+
+
+// local
+// require('dotenv').config()
+// const express = require('express')
+// const cors = require('cors')
+// const { initDB } = require('./db')
+// const routes = require('./routes')
+
+// const app = express()
+
+// app.use(express.json())
+// app.use(cors())
+// app.use('/', routes)
+
+// initDB().catch(err => {
+//   console.error('DB init error:', err)
+//   process.exit(1)
+// })
+
+// const PORT = process.env.PORT || 3001
+// app.listen(PORT, () => console.log(`running on ${PORT}`))
